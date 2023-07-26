6825fd8a777626518a29c5fbae3105cdc8cee819
吴波 <913721086@qq.com>
2023-07-26 15:48:48 +0800

    LoadBalancerClient

--- spring-cloud-study/borrow-service/src/main/java/com/scs/config/BeanConfiguration.java
+++ spring-cloud-study/borrow-service/src/main/java/com/scs/config/BeanConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(name = "userservice", configuration = LoadBalancerConfig.class)
 public class BeanConfiguration {
     @Bean
     @LoadBalanced
